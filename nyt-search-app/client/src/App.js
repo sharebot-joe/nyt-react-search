86ac8bc04ab5556fc3fb83747ee1739dc0cf57b6
sharebot-joe <josephikim2@gmail.com>
2018-07-23 23:28:14 -0700

    skeleton files

---
+++ nyt-search-app/client/src/App.js
@@ -0,0 +1,50 @@
+import React, {Component} from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+/*
+import Books from "./pages/Books";
+import Detail from "./pages/Detail";
+import NoMatch from "./pages/NoMatch";
+import Nav from "./components/Nav";
+*/
+
+class App extends Component {
+  state = {users: []}
+
+  componentDidMount() {
+    fetch('/users')
+      .then(res => res.json())
+      .then(users => this.setState({ users }));
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Users</h1>
+        {this.state.users.map(user =>
+          <div key={user.id}>{user.username}</div>
+        )}
+      </div>
+    );
+  }
+}
+
+export default App;
+
+/* Hidden backend testing
+/*
+const App = () => (
+  <Router>
+    <div>
+      <Nav />
+      <Switch>
+        <Route exact path="/" component={Books} />
+        <Route exact path="/books" component={Books} />
+        <Route exact path="/books/:id" component={Detail} />
+        <Route component={NoMatch} />
+      </Switch>
+    </div>
+  </Router>
+);
+
+export default App;
+*/
